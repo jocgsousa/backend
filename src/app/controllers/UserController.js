545b769dda10fba8af0a16d76c66d1083a86107d
jcogsousa <gregoriociacom@gmail.com>
2020-06-30 17:09:33 -0300

    Estrutura atualizada

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+
+class UserController {
+  async store(request, response) {
+    const userExists = await User.findOne({ where: { email: request.body.email } });
+    if (userExists) {
+      return response.status(400).json({ error: 'Conta de e-mail já cadastrada' });
+    }
+    const { id, name, email } = await User.create(request.body);
+    return response.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+export default new UserController();
